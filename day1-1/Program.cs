b6cee5b27e6421471d70476d859df03f41d9595e
bls220 <bls220@vt.edu>
2020-12-01 23:49:03 -0500

    Day 1

---
+++ day1-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace day1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            int[] values = lines.Select(line => Int32.Parse(line)).ToArray();
+            foreach (var valueX in values)
+            {
+                foreach (var valueY in values)
+                {
+                    if (valueX + valueY == 2020)
+                    {
+                        Console.WriteLine(valueX * valueY);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ day1-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace day1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            int[] values = lines.Select(line => Int32.Parse(line)).ToArray();
+            foreach (var valueX in values)
+            {
+                foreach (var valueY in values)
+                {
+                    foreach (var valueZ in values)
+                    {
+                        if (valueX + valueY + valueZ == 2020)
+                        {
+                            Console.WriteLine(valueX * valueY * valueZ);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

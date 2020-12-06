b305df5255b85deb55719c5abec92b5000ea99c6
bls220 <bls220@vt.edu>
2020-12-06 15:19:20 -0500

    Day6

---
+++ day6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groups = System.IO.File.ReadAllText("input.txt").Split("\n\n");
+
+            // Answer 1
+            //var groupAnswers = groups.Select(group => group.Distinct().Where(c => c != '\n').Count());
+
+            var groupAnswers = groups
+            .Select(group =>
+                group.Split('\n')
+                .Where(person => !String.IsNullOrWhiteSpace(person)))
+            .Select(people =>
+                people.Select(person => person.AsEnumerable())
+                .Aggregate((answers1, answers2) => answers1.Intersect(answers2))
+                .Count());
+
+            var sum = groupAnswers.Aggregate((total, groupSum) => total + groupSum);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
